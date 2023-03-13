2fbb34a39d8dc1aa35a99340b1b1ea6b0b3a3579
Piorek1994 <pprus94@gmail.com>
2023-03-13 21:08:42 +0100

    custom animation

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React from 'react'
+// images
+import Image from '../assets/avatar.png'
+//icons
+import { FaGithub, FaYoutube, FaDribbble } from 'react-icons/fa'
+//type animation
+import { TypeAnimation } from 'react-type-animation'
+//motion
+import { motion } from 'framer-motion'
+//variants
+import { fadeIn } from '../variants'
 
 const Banner = () => {
-  return <div className='section'>Banner</div>;
-};
+	return (
+		<section className='section' id='home'>
+			<div className='container mx-auto'>
+				<div>
+					{/* text */}
+					<div>
+						<h1>
+							PIOTR <span>PRUS</span>
+						</h1>
+						<div>
+							<span>I am a</span>
+							<TypeAnimation sequence={['Developer', 2000, 'Designer', 2000, 'YouTuber', 2000]} speed={50} className='text-accent'/>
+						</div>
+					</div>
+					{/* text */}
+					<div>
+						<img src={Image} alt='' />
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
 
-export default Banner;
+export default Banner
